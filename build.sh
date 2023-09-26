843effc009509dadea501cc85c1a8f8593d4de06
Martin Clauß <51541424+martinclauss@users.noreply.github.com>
2023-09-26 15:50:37 +0200

    Make LLVM 11 compile under Ubuntu 20.04+

--- build.sh
+++ build.sh
@@ -37,6 +37,7 @@ pushd llvm_tools
 wget -O llvm-11.0.0.src.tar.xz https://github.com/llvm/llvm-project/releases/download/llvmorg-11.0.0/llvm-11.0.0.src.tar.xz
 tar -xf llvm-11.0.0.src.tar.xz
 mv      llvm-11.0.0.src        llvm
+sed -i -e 's/#include <vector>/#include <limits>\n#include <vector>/' ./llvm/utils/benchmark/src/benchmark_register.h
 
 wget -O  clang-11.0.0.src.tar.xz https://github.com/llvm/llvm-project/releases/download/llvmorg-11.0.0/clang-11.0.0.src.tar.xz
 tar -xf  clang-11.0.0.src.tar.xz
